2cc7a436bf1f6099930e9c9643cace3269b7fc6a
Callum <callum.hopkins@protonmail.com>
2022-01-06 19:13:28 +1030

    Created file, wrote notes from https://javascript.info/variables

---
+++ variables.js
@@ -0,0 +1,39 @@
+/*To create a  variable*/
+let = "variable"; 
+
+/*To name a variable*/
+variable = "variableName"; 
+
+/*To create and name a variable*/
+let variable = "variableName";
+
+/*To create and/or name multiple variables at once*/
+let variable = "variableName",
+    VariableNameOnly;
+
+/*To copy data from one variable to another*/
+let dataBeingCopied = "data";
+let variableToCopyInto;
+
+/*To declare a variable that cannot be changed after creation*/
+const myBirthday = "21.02.2000";
+
+/*Always use upper case when defining always known variables such as colours*/
+const COLOUR_RED = "#F00"
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+//Copy 'data' from dataBeingCopied into variableToCopyInto
+variableToCopyInto = dataBeingCopied;
+//First variable (variableToCopyInto) will copy data from second variable (dataBeingCopied)
+
+/*Variable names can only contain letters, digits, or the symbols $ and _*/
+//Cannot begin with digit, cannot use hyphens, should start with lowercase
+//Variables are case sensitive
+//List of reserved words that cannot be used, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords
+let $
+let _
+let variableName2
+
+
