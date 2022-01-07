79e9afa6f994537cbc843ab1899ec85c3531f013
Callum <callum.hopkins@protonmail.com>
2022-01-07 21:16:49 +1030

    Took notes from https://www.w3schools.com/js/js_arithmetic.asp

---
+++ arithmetic/arithmetic.js
@@ -0,0 +1,55 @@
+/*Arithmetic operators*/
+
+/*https://www.w3schools.com/js/js_arithmetic.asp*/
+
+//Use BIDMAS//
+
+//Can substitute numbers with variables
+let x = 5,
+    y = 5;
+x + y;
+10
+
+//Addition
+5 + 5;
+10
+
+//Subtraction
+5 - 5;
+0
+
+//Multiplication
+5 * 5;
+25
+
+//Exponentation - Power to
+5 ** 5;
+3125
+
+//Division
+5 / 5;
+1
+
+//Modulus (remainder)
+5 % 5;
+0
+//Can evenly divide 4 into 10 twice, leaving 2 remainder
+// 10/4=8 with 2 remainder
+10 % 4;
+2
+
+//Increment - increases number by 1
+5++;
+6
+
+//Decrement - decreases number by 1
+5--;
+4
+
+//Shorthand
+//Where x = 5
+x = x + 10;
+15
+//Can be written as
+x += 10;
+15
