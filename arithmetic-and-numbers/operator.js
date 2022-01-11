613293e8d62877d7c9c1ccfc5a6e4dadcfe2bf4b
Callum <callum.hopkins@protonmail.com>
2022-01-11 18:41:00 +1030

    Added comparison operators and arithmetic operators

--- arithmetic-and-numbers/operator.js
+++ arithmetic-and-numbers/operator.js
@@ -1,3 +1,27 @@
+/*
+Arithmetic Operators
+*/
+
++ //Adds two numbers together
+- //Subtracts right number from left number
+* //Multiplies two numbers together
+/ //Divides left number by the right number
+% //Returns remainder leftover after dividing the left number into the right in whole numbers
+** //Equivalent to 'the power of'
+(+ or - or * or * or / or % or **)= //Shorthand for e.g. x = x + 4
+
+
+/*
+Comparison Operators
+*/
+./comparison-operators.png
+== and != //Do not consider datatype
+=== and !== //Do consider datatype
+
+"1" == 1; true
+"1" === 1: false
+
 typeof //Shows datatype
 const //Sets unchangeable variable
 let //Sets changeable variable
+Number(variable) //To temporarily convert string into number
