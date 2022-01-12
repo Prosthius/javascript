540e61f3e410cc63682419c31fa6a41d9f8d7862
Callum <callum.hopkins@protonmail.com>
2022-01-12 21:52:14 +1030

    Completed 'the typeof operator'

--- data-types/data-types.js
+++ data-types/data-types.js
@@ -103,3 +103,28 @@ Objects and symbols
 
 
 
+/*
+The typeof operator
+*/
+//Returns the type of the argument
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3)
+
+//1.Math is a built-in object that provides mathematical operations. 
+//2.Null is not an object, an error of typeof
+//3.No "function" type, belongs to the object type
