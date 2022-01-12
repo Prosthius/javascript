6c29c54e4bd37d63ad31d809eaf4f3f0f2d5afa8
Callum <callum.hopkins@protonmail.com>
2022-01-12 19:12:42 +1030

    Notes on BigInt

--- data-types/data-types.js
+++ data-types/data-types.js
@@ -15,3 +15,13 @@ Infinity
 NaN
 //Will propgate to all further expressions
 
+
+/*
+BigInt
+*/
+//Number type cannot represent integer values larger than (2^53-1)
+//or 9,007,199,254,740,991
+//Created by appending 'n' to the end of an integer
+const bigInt = 1234567890123456789012345678901234567890n;
+
+
