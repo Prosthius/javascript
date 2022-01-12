fa845fd303706966bdea779fa841c2a9929f3617
Callum <callum.hopkins@protonmail.com>
2022-01-12 19:00:55 +1030

    Completed notes on number type

---
+++ data-types/data-types.js
@@ -0,0 +1,17 @@
+/*
+Number
+*/
+let n = 123;
+n = 12.345;
+
+//Special numeric values:
+
+//Infinity - division by zero
+1 / 0;
+Infinity
+
+//NaN - incorrect or undefined mathematical operation
+"word" / 2 + 5;
+NaN
+//Will propgate to all further expressions
+
