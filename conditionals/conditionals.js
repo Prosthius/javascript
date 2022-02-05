4611e6a6f1775772fb49d695cab9607387459691
Prosthius <ietvntpd@duck.com>
2022-02-05 16:49:50 +1100

    Added notes from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals on switch and conditional operators

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -4,6 +4,7 @@ if;     //To specify a block of code to be executed, if a specified condition is
 else;   //To specify a block of code to be executed, if the same condition is false
 else if;//To specify a new condition to test, if the first condition is false
 switch; //To specify many blocks of code to be executed
+(condition) ? // Ternary or conditional operator: Tests a piece of code and returns one string if the conditon returns true, or another if it returns false
 
 
 /*if*/
@@ -50,3 +51,90 @@ if (time < 10) {
   } else {
     greeting = "Good evening";
   }
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals
+
+/*switch*/
+switch (expression) {
+  case choice1:
+    run this code
+    break;  //Stops the browser from executing code if the previous code matches the expression/value, moves on to below the switch statement 
+
+  case choice2:
+    run this code instead
+    break;
+
+  // include as many cases as you like
+
+  default:  //The default option to run if none of the choices match. It is optional
+    actually, just run this code
+}
+//Example
+//Button in HMTL with options for weather
+
+//HTML
+<label for="weather">Select the weather type today: </label>
+<select id="weather">
+  <option value="">--Make a choice--</option>
+  <option value="sunny">Sunny</option>
+  <option value="rainy">Rainy</option>
+  <option value="snowing">Snowing</option>
+  <option value="overcast">Overcast</option>
+</select>
+<p></p>
+
+const select = document.querySelector('select');
+const para = document.querySelector('p'); 
+
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  switch (choice) {
+    case 'sunny':
+      para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+      break;
+    case 'rainy':
+      para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+      break;
+    case 'snowing':
+      para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+      break;
+    case 'overcast':
+      para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+      break;
+    default:
+      para.textContent = '';
+  }
+}
+
+
+/*Ternary or conditional operator*/
+( condition ) ? run this code : run this code instead
+//Example
+let greeting = ( isBirthday ) ? 'Happy birthday Mrs. Smith — we hope you have a great day!' : 'Good morning Mrs. Smith.';
+//If true, returns first message, if false, returns second message
+
+//Example - more complex
+//HTML
+<label for="theme">Select theme: </label>
+<select id="theme">
+  <option value="white">White</option>
+  <option value="black">Black</option>
+</select>
+
+<h1>This is my website</h1>
+
+
+const select = document.querySelector('select');
+const html = document.querySelector('html');
+document.body.style.padding = '10px';
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.addEventListener('change', () => ( select.value === 'black' ) ? update('black','white') : update('white','black'));
